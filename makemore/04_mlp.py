e6769bc2cf543a3068bb846e63995c122cdd72cd
Rui Hu <code@mrhooray.com>
2025-09-14 10:29:50 -0700

    makemore - mlp training

--- makemore/04_mlp.py
+++ makemore/04_mlp.py
@@ -24,25 +24,26 @@ for name in names:
 
 X = torch.tensor([[stoi[ch] for ch in prev] for prev, _ in data])
 Y = torch.tensor([stoi[next] for _, next in data])
-EMB = torch.randn((27, 2))
-
 print(f"X shape {X.shape}")
 print(f"Y shape {Y.shape}")
+
+g = torch.Generator().manual_seed(24)
+EMB = torch.randn((27, 2), generator=g)
 print(f"EMB shape {EMB.shape}")
 
 X_onehot = F.one_hot(X, num_classes=27).float()
 print(f"X_onehot shape {X_onehot.shape}")
 print("X_onehot @ EMB equals to EMB[X]?", torch.allclose(X_onehot @ EMB, EMB[X]))
 
-W1 = torch.randn((6, 100))
-B1 = torch.randn((100))
+W1 = torch.randn((6, 100), generator=g)
+B1 = torch.randn((100), generator=g)
 
 emb = X_onehot @ EMB
-h = emb.view(-1, 6) @ W1 + B1
+h = torch.tanh(emb.view(-1, 6) @ W1 + B1)
 print(f"h shape {h.shape}")
 
-W2 = torch.randn((100, 27))
-B2 = torch.randn((27))
+W2 = torch.randn((100, 27), generator=g)
+B2 = torch.randn((27), generator=g)
 logits = h @ W2 + B2
 print(f"logits shape {logits.shape}")
 
@@ -52,3 +53,28 @@ print(f"probs shape {probs.shape}")
 
 probs_label = probs[torch.arange(X.shape[0]), Y]
 print(f"probs_label shape {probs_label.shape}")
+
+params = [EMB, W1, B1, W2, B2]
+params_count = sum(p.nelement() for p in params)
+print(f"params_count {params_count}")
+
+for p in params:
+    p.requires_grad = True
+    p.data = torch.randn(p.shape, generator=g)
+
+n_epochs = 128
+learning_rate = 0.16
+for _ in range(n_epochs):
+    emb = EMB[X]
+    h = torch.tanh(emb.view(-1, 6) @ W1 + B1)
+    logits = h @ W2 + B2
+    loss = F.cross_entropy(logits, Y)
+    print(f"loss {loss.item()}")
+    for p in params:
+        p.grad = None
+
+    loss.backward()
+
+    for p in params:
+        if p.grad is not None:
+            p.data += -learning_rate * p.grad
